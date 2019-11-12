a9e5e83883e1795730bcee58549123b16df16d94
Hyeon Hong <hyeonshong@gmail.com>
2019-11-13 08:10:53 +0900

    Install and configure SCSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     author: 'Hyeon Hong'
   },
   plugins: [
+    'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-filesystem',
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, graphql, useStaticQuery } from 'gatsby';
 
 import Layout from '../components/layout';
-// import blogStyles from './blog.module.scss'
+import blogStyles from './blog.module.scss';
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -26,12 +26,10 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      {/* <ol className={blogStyles.posts}> */}
-      <ol>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map((edge) => {
           return (
-            // <li className={blogStyles.post}>
-            <li>
+            <li className={blogStyles.post}>
               <Link to={`/blog/${edge.node.fields.slug}`}>
                 <h2>{edge.node.frontmatter.title}</h2>
                 <p>{edge.node.frontmatter.date}</p>
