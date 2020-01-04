cb254d3090942151e6302b91d025a4fe0a45cf93
Hyeon Hong <hyeonshong@gmail.com>
2020-01-04 17:35:15 +0900

    Change primejs theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,8 @@
-require('prismjs/themes/prism-solarizedlight.css');
+require('prismjs/themes/prism-tomorrow.css');
+// require('prismjs/themes/prism.css'); // default theme
+// require('prismjs/themes/prism-coy.css');
+// require('prismjs/themes/prism-dark.css');
+// require('prismjs/themes/prism-funky.css');
+// require('prismjs/themes/prism-okaidia.css');
+// require('prismjs/themes/prism-twilight.css');
+// require('prismjs/themes/prism-solarizedlight.css');
